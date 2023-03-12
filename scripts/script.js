bccbf5be7cb67f7a22cf3803e949d6499f5cda55
Elena-80 <elena.chumakova80@yandex.ru>
2023-03-12 17:56:50 +0300

    Margins corrected

--- scripts/script.js
+++ scripts/script.js
@@ -2,14 +2,14 @@ let container = document.querySelector('.root');
 let editButton = container.querySelector('.profile__edit-button');
 let popupElement = container.querySelector('.popup');
 let closeIcon = popupElement.querySelector('.popup__close-button');
-let formElement = popupElement.querySelector('.popup__form'); 
-let profileName = container.querySelector('.profile__name'); 
+let formElement = popupElement.querySelector('.popup__form');
+let profileName = container.querySelector('.profile__name');
 let profileJob = container.querySelector('.profile__profession');
-let nameInput =  formElement.querySelector('.popup__name');
-let jobInput = formElement.querySelector('.popup__profession');
+let nameInput =  formElement.querySelector('.popup__text_name');
+let jobInput = formElement.querySelector('.popup__text_profession');
 
 function editProfile() {
-    
+
     popupElement.classList.add('popup_opened');
     jobInput.value = profileJob.textContent;
     nameInput.value = profileName.textContent;
